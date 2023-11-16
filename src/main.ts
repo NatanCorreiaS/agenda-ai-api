acba9df817a5153773a5403f205f2d29703e4dff
Natan <natancorreiadev@gmail.com>
2023-11-16 13:00:06 -0300

    colocando o mongodb connection pro host

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,17 @@ import router from "./routers";
 
 dotenv.config();
 
+// Connecting to the database
+
+mongoose.connect(process.env.MONGO_URL);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+
+db.once("open", () => {
+  console.log("Connected to the database");
+});
+
 // Creating the app
 const app = express();
 
@@ -30,15 +41,4 @@ const server = http.createServer(app);
 // Listening to the server
 server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
-});
-
-// Connecting to the database
-
-mongoose.connect(process.env.MONGO_URL);
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-
-db.once("open", () => {
-  console.log("Connected to the database");
 });
