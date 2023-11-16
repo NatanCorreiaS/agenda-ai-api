62efd6d1d912d671fc4a0b26d267ca7f7a6a3bed
Natan <natancorreiadev@gmail.com>
2023-11-16 13:11:02 -0300

    dist deletado

--- dist/routers/authRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const auth_1 = require("../controllers/auth");
-exports.default = (router) => {
-    router.post("/api/register", auth_1.register);
-    router.post("/api/login", auth_1.login);
-    router.post("/api/session", auth_1.findBySession);
-};
-//# sourceMappingURL=authRouter.js.map
